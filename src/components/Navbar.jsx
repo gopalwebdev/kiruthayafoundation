d544c07a5ee2fb4b0d0b53af940eaaaa013caf2f
Gopal Narayanan <gopalwebdev@gmail.com>
2022-05-28 23:17:36 +0530

    Initial commit with navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import Logo from '../assets/images/logo.png'
+
+function Navbar() {
+  return (
+   <>
+   <div className="container mx-auto px-6 py-6 flex justify-between items-center md:px-20 lg:px-44">
+       <img src={Logo} alt="Kiruthaya Foundation Logo" width={150}/>
+       <button className="h-8 w-24 rounded-lg text-sm font-semibold text-opacity-95 bg-green-500 text-white shadow-lg shadow-green-500/50">Gallery</button>
+   </div>
+   </>
+  )
+}
+
+export default Navbar
